19599f0a0c49c097a46bc085c71eece079bba23b
Alberto Ramiro Durán Pérez <aduranperez@edu.xunta.es>
2026-03-08 02:07:43 +0100

    Primer despliegue de mi sitio RMarkdownsddsssejn

---
+++ practica 2.R
@@ -0,0 +1,54 @@
+9/2
+x=6
+print(x)
+5==6
+x=7
+x
+x==8
+
+
+x=4
+x
+78==x
+4==x
+
+x = 50
+x!= 89
+x= 50
+y = 100
+x==
+x!=y
+x<y
+
+20<80 | 45 <= 25
+
+
+temperaturas <- c(23, 21, 27, 20, 17)
+mean(temperaturas)
+median(temperaturas)
+range(temperaturas)
+sort(temperaturas)
+temperaturas
+temperaturas_ord = sort(temperaturas)
+temperaturas
+temperaturas_ord
+summary(temperaturas)
+min(temperaturas)
+which.min(temperaturas)
+
+temperatura_doble = 2*temperaturas
+temperatura_doble
+
+temperaturas == 21 
+
+x = 5.7
+x = 3L
+x = "Nicolás"
+
+enviado  =  TRUE
+
+installed.packages("clustcurv")
+library(clustcurv)
+
+
+data()
